d08c74d9ba774553c7bfba8094d099440412f15b
Arbab(AB) <arbabaliabforever@gmail.com>
2021-05-21 13:13:08 +0500

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -6,7 +6,7 @@ TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
